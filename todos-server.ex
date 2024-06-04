b40008413a45d17643cb6c5546b7ecc695575468
Emiljan Dusha <emiljanthegreat@gmail.com>
2024-06-04 08:45:39 +0200

    feat: todo-server wip

--- todos-server.ex
+++ todos-server.ex
@@ -1,18 +1,60 @@
 defmodule TodosServer do
-  # will handle state init
-  # will handle starting the server
   # will handle call messages
   # will handle cast messages
   # will have the interface functions
-  def start(callback_module) do
+
+  def start do
+    ServerProcess.start()
+  end
+
+  def init do
+    TodoList.new()
+  end
+
+  def entries(pid, date) do
+    ServerProcess.call(pid, {:entries, date})
+  end
+
+  def add_todo(pid, todo) do
+    ServerProcess.cast(pid, {:add_todo, todo})
+  end
+
+  def remove_todo(pid, id) do
+    ServerProcess.cast(pid, {:remove_todo, id})
+  end
+
+  def update_todo(pid, id, update_fn) do
+    ServerProcess.call(pid, {:update_todo, id, update_fn})
   end
 end
 
 defmodule ServerProcess do
-  # will have the loop function
-  # will have cast function
-  # will have call function
-  # will have start function
+  def start(callback_module) do
+    spawn(fn -> loop(callback_module, callback_module.init()) end)
+  end
+
+  def loop(callback_module, state) do
+    new_state =
+      receive do
+        message -> callback_module.handle_message(message)
+      end
+
+    loop(callback_module, new_state)
+  end
+
+  def call(pid, message) do
+    send(pid, self(), message)
+
+    receive do
+      {:response, message} -> message
+    after
+      5000 -> IO.puts("no response")
+    end
+  end
+
+  def cast(pid, message) do
+    send(pid, message)
+  end
 end
 
 defmodule TodoList do
