c652c6dab369a8a396b827f8defd8e973f69e232
Emiljan <emiljan.dusha@sisal.it>
2025-01-30 11:48:46 +0100

    feat: created Todo Application

--- todo_cache/test/todo/cache_test.exs
+++ todo_cache/test/todo/cache_test.exs
@@ -2,16 +2,14 @@ defmodule Todo.CasheTest do
   use ExUnit.Case
 
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
-    todo1_pid = Todo.Cache.server_process(cache, :todo1)
+    todo1_pid = Todo.Cache.server_process(:todo1)
 
-    assert todo1_pid != Todo.Cache.server_process(cache, :todo2)
-    assert todo1_pid == Todo.Cache.server_process(cache, :todo1)
+    assert todo1_pid != Todo.Cache.server_process(:todo2)
+    assert todo1_pid == Todo.Cache.server_process(:todo1)
   end
   
   test "to-do operations" do
-    {:ok, cache} = Todo.Cache.start()
-    todo1 = Todo.Cache.server_process(cache, :todo1)
+    todo1 = Todo.Cache.server_process(:todo1)
     Todo.Server.add_todo(todo1, %{date: ~D[2020-12-20], title: "todo1 at todo1"})
 
     entries = Todo.Server.entries(todo1, ~D[2020-12-20])
