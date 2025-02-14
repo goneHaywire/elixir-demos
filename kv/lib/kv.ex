ec4c3b1a07e4d64437aed178dcfdece10a650a7d
Emiljan Dusha <emiljanthegreat@gmail.com>
2025-02-14 14:06:41 +0100

    feat: added KV.Supervisor & Application module

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -1,18 +1,11 @@
 defmodule KV do
+  use Application
   @moduledoc """
   Documentation for `KV`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KV.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  
+  @impl true
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: Supervisor)
   end
 end
